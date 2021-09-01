0e860496ce490d5ca9b741b612440720e18df656
Shairyar Baig <shairyarbaig@gmail.com>
2021-09-01 17:52:30 +0500

    Integrate appsignal

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@
 import { example } from '@/js/example'
 
 // Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
+// import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
 
 // Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
+// const logo = document.createElement('img')
+// logo.src = webpackLogo
 
 const heading = document.createElement('h1')
 heading.textContent = example()
 
 const app = document.querySelector('#root')
-app.append(logo, heading)
+app.append(heading)
--- src/js/example.js
+++ src/js/example.js
@@ -1,3 +1,17 @@
-export const example = () =>
-  `Sensible webpack 5 boilerplate using Babel and PostCSS with a hot dev server 
-  and an optimized production build.`
+import Appsignal from '@appsignal/javascript'
+
+const appsignal = new Appsignal({ key: 'YOUR-FRONTEND-API-KEY' })
+
+export const example = () => `Hello World.`
+
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+  const param = JSON.parse(error.message).foo
+  const span = appsignal.createSpan()
+
+  span.setTags({ tag: param }).setError(error)
+  appsignal.send(span)
+  return false
+}
+throw new Error(
+  JSON.stringify({ message: 'Some new exception message', foo: 'bar' })
+)
