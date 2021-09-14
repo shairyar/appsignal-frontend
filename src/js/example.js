d659218924b4dedd3032de4728a5a8bb8a8ae323
Shairyar Baig <shairyarbaig@gmail.com>
2021-09-14 14:12:50 +0500

    Add ignoreErrors config example

--- src/js/example.js
+++ src/js/example.js
@@ -1,17 +1,19 @@
 import Appsignal from '@appsignal/javascript'
 
-const appsignal = new Appsignal({ key: 'YOUR-FRONTEND-API-KEY' })
+const ignore = [/ResizeObserver/gm, /Loading chunk/gm, /msoCommentHide/gm]
+
+const appsignal = new Appsignal({
+  key: 'YOUR-FRONTEND-API-KEY',
+  ignoreErrors: ignore,
+})
 
 export const example = () => `Hello World.`
 
 window.onerror = function (msg, url, lineNo, columnNo, error) {
-  const param = JSON.parse(error.message).foo
   const span = appsignal.createSpan()
 
-  span.setTags({ tag: param }).setError(error)
+  span.setTags({ tag: 'param' }).setError(error)
   appsignal.send(span)
   return false
 }
-throw new Error(
-  JSON.stringify({ message: 'Some new exception message', foo: 'bar' })
-)
+throw new Error('This exception will be ignored ResizeObserver')
